9ec30f3fc5038df15f1a81ca90cbda1827a9180a
Youssef-GitHub <75904052+Youssef-GitHub@users.noreply.github.com>
2021-12-26 18:56:44 +0000

    Create 21 - Solution validation.py

---
+++ 21 - Solution validation.py
@@ -0,0 +1,18 @@
+def validate(code):
+    if "def" not in code:
+        return "missing def"
+    elif ":" not in code:
+        return "missing :"
+    elif "(" not in code:
+        return "missing paren"
+    elif ")" not in code:
+        return "missing paren"
+    elif ('()' in code) and ("'()'" not in code):
+        return "missing param"
+    elif "    " not in code:
+        return "missing indent"
+    elif "validate" not in code:
+        return "wrong name"
+    elif "return" not in code:
+        return "missing return"
+    return True
